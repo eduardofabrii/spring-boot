50144f4e63678bbbab6d9375ee5f9f3b4a6c9511
Eduardo Fabri <eduardohfabri@gmail.com>
2025-01-09 10:17:55 -0300

    Inicializando projeto Spring

---
+++ curso-udemy/src/test/java/com/curso/udemy/curso_udemy/CursoUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.udemy.curso_udemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
